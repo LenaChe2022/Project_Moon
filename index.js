84cce3598bfb15690dda9fe2075a947e5d8b3337
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2025-08-01 15:31:23 -0700

    clean folder, change navbar arrows vew and text format

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const API_URL = "https://api.openweathermap.org/data/3.0/onecall?lat=47.674&lon=
 const yourAPIKey = apiKey;
 
 const mockData = JSON.parse(fs.readFileSync("mockData.json"));
-//const dayDescription = JSON.parse(fs.readFileSync("day_description.json"));
 const dayDescription = JSON.parse(fs.readFileSync("myDayText.json"));
 //console.log(dayDescription.nameRu[0]);
 
@@ -32,15 +31,16 @@ app.use(express.static("public"));
 
 
 //mock data response for developing UI without API calls
-app.get("/", (req, res) => {
-    const moonDate = moonDay;
-    const i = moonDay - 1;
-    const atTime = localTimeMoonRise;
-    const myDay = dayDescription;
+// app.get("/", (req, res) => {
+//     const moonDate = moonDay;
+//     const i = moonDay - 1;
+//     const atTime = localTimeMoonRise;
+//     const myDay = dayDescription;
 
-    res.render("index.ejs", {moonDay: moonDate, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]})
-});
+//     res.render("index.ejs", {moonDay: moonDate, homeNav: 1, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]})
+// });
 
+//Previous day using mock data:
 app.get("/previous", (req,res) => {
     var moonDate = 0;
     var i = 0;
@@ -53,39 +53,66 @@ app.get("/previous", (req,res) => {
     }
     const myDay = dayDescription;
     const atTime = 0;
+
 console.log("yesterday was " + moonDate + " moon Day" );
 
-res.render("index.ejs", {moonDay: moonDate, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]})
+res.render("index.ejs", {moonDay: moonDate, homeNav: 0, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]})
+});
+
+app.get("/next", (req, res) => {
+    const moonFase = mockData.daily[1].moon_phase;
+    const moonRise = mockData.daily[1].moonrise;
+    const locationName = mockData.timezone;
+
+    const moonriseTime = new Date(moonRise * 1000);
+    const localTimeMoonRise = moonriseTime.toLocaleTimeString([], { hourCycle: 'h23', hour: '2-digit', minute: '2-digit', timeZone: locationName });
+
+    const moonDay = Math.floor((moonFase * 29.53) +1);
+
+    console.log(`Next Moon Day ${moonDay} will start in ${ localTimeMoonRise} tomorrow`);
+
+    const moonDate = moonDay;
+    const i = moonDay - 1;
+    const atTime = localTimeMoonRise;
+    const myDay = dayDescription;
+
+    
+
+    res.render("index.ejs", {moonDay: moonDate, homeNav: 0, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]})
+
+
 });
 
 
 //Code with live API request (1000 requests per day):
 
-// app.get("/",async(req,res) => {
-//     try {
-//         const response = await axios.get(API_URL + yourAPIKey);
-//         //console.log(response.data);
+app.get("/",async(req,res) => {
+    try {
+        const response = await axios.get(API_URL + yourAPIKey);
+        //console.log(response.data);
 
-//         const moonFase = response.data.daily[0].moon_phase;
-//         const moonRise = response.data.daily[0].moonrise;
-//         const locationName = response.data.timezone;
-//         const moonriseTime = new Date(moonRise * 1000);
-//         const localTimeMoonRise = moonriseTime.toLocaleTimeString([], { hourCycle: 'h23', hour: '2-digit', minute: '2-digit', timeZone: locationName });
-//         const moonDate = Math.floor((moonFase * 29.53) +1);
+        const moonFase = response.data.daily[0].moon_phase;
+        const moonRise = response.data.daily[0].moonrise;
+        const locationName = response.data.timezone;
+        const moonriseTime = new Date(moonRise * 1000);
+        const localTimeMoonRise = moonriseTime.toLocaleTimeString([], { hourCycle: 'h23', hour: '2-digit', minute: '2-digit', timeZone: locationName });
+        const moonDate = Math.floor((moonFase * 29.53) +1);
 
-//         console.log(`Moon Day ${moonDate} started in ${ localTimeMoonRise} today`);
+        console.log(`Moon Day ${moonDate} started in ${ localTimeMoonRise} today`);
 
-//         const i = moonDate - 1;
-//         const atTime = localTimeMoonRise;
-//         const myDay = dayDescription;
+        const i = moonDate - 1;
+        const atTime = localTimeMoonRise;
+        const myDay = dayDescription;
 
     
-//         res.render("index.ejs", {moonDay: moonDate, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]});
-//     } catch (error) {
-//         console.error("Failed to make request: ", error.message);
-//         res.status(500).send("Failed to get Moon Day. Please try again later.")
-//     }
-// });
+        res.render("index.ejs", {moonDay: moonDate, homeNav: 1, moonTime: atTime, moonDayName: myDay.nameRu[i], daySummary: myDay.summaryRu[i], todo: myDay.todoRu[i], avoid: myDay.avoidRu[i], fullDescription: myDay.descriptionRu[i]});
+    } catch (error) {
+        console.error("Failed to make request: ", error.message);
+        res.status(500).send("Failed to get Moon Day. Please try again later.")
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
